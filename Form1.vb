a6a487fbbc686b3a55df6e8313b1154112378dc1
yasmin506 <yasmine.mzoughi@isgb.ucar.tn>
2025-11-05 22:40:01 +0100

    ex1-even

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim nom As String = InputBox("Entrez votre nom :", "Saisie du nom")
+        Dim age As String = InputBox("Entrez votre âge :", "Saisie de l'âge")
+
+
+        MessageBox.Show("Bonjour " & nom & ", vous avez " & age & " ans.", "Message")
+    End Sub
+End Class
+
+
